925327de5f9bc75750c71272cfc9baa0d5615f05
guiferviz <guiferviz@gmail.com>
2020-04-28 22:12:52 +0200

    Updating setup.py. Using Linux endlines, updating metadata and importing packages.

--- setup.py
+++ setup.py
@@ -1,35 +1,47 @@
 
-from os import listdir
-from os.path import join
-
 from setuptools import setup, find_packages
-try: # for pip >= 10
-    from pip._internal.req import parse_requirements
-except ImportError: # for pip <= 9.0.3
-    from pip.req import parse_requirements
+from pip._internal.req import parse_requirements
 
 
 PACKAGE_NAME = "aidictive"
+DESCRIPTION = "An addictive AI library."
+KEYWORDS = "python ai ml framework dl machine deep learning deep pytorch"
+AUTHOR = "guiferviz"
+AUTHOR_EMAIL = "guiferviz@gmail.com"
+LICENSE = "Copyright " + AUTHOR
+URL = "https://github.com/guiferviz/aidictive"
 
+
+print("Project:", PACKAGE_NAME)
 
 # Creates a __version__ variable.
 with open(PACKAGE_NAME + "/_version.py") as file:
     exec(file.read())
+print("Version:", __version__)
 
 # Read requirements.
 req = parse_requirements("requirements.txt", session="hack")
+for i in req:
+    print(dir(i))
+    print(i.requirement)
 REQUIREMENTS = [str(ir.req) for ir in req]
 print("Requirements:", REQUIREMENTS)
 
+# Install all packages in the current dir except tests.
+PACKAGES = find_packages(exclude=["tests", "tests.*"])
+print("Packages:", PACKAGES)
+
 setup(name=PACKAGE_NAME,
       version=__version__,
-      description="Addictive AI Library.",
+      description=DESCRIPTION,
       long_description=open("README.md").read(),
       long_description_content_type="text/markdown",
-      keywords="ml framework",
-      author="guiferviz",
-      license="Copyright guiferviz",
-      packages=find_packages(),
+      url=URL,
+      keywords=KEYWORDS,
+      author=AUTHOR,
+      author_email=AUTHOR_EMAIL,
+      license=LICENSE,
+      packages=PACKAGES,
       install_requires=REQUIREMENTS,
       entry_points={
           "console_scripts": [
